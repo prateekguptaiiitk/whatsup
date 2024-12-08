3ae31bd23c9002303beb80acd402effdec5b0838
prtk_gupta <prateekjpg@gmail.com>
2024-12-08 16:41:40 +0530

    Add register submit logic

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import axios from "axios";
+import { UserContext } from "./UserContext";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const {setUsername: setLoggedInUsername, setId} = useContext(UserContext)
+
+  async function userRegister(ev) {
+    ev.preventDefault();
+    const {data} = await axios.post("/register", { username, password });
+    setLoggedInUsername(username)
+    setId(data.id)
+  }
+
   return (
     <div className="bg-blue-50 h-screen flex items-center">
-      <form className="w-64 mx-auto mb-12">
+      <form className="w-64 mx-auto mb-12" onSubmit={userRegister}>
         <input
           type="text"
           value={username}
@@ -15,7 +26,7 @@ export default function Register() {
           className="block w-full rounded-sm p-2 mb-2 border"
         />
         <input
-          type="text"
+          type="password"
           value={password}
           onChange={(ev) => setPassword(ev.target.value)}
           placeholder="password"
